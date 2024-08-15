fe6e6a8087f7faffd5584f9f414311302dfcc12d
oluwatrillions <ajosemichaeloluwatobi@yahoo.com>
2024-08-15 15:37:20 +0100

    ...

--- src/app/page.js
+++ src/app/page.js
@@ -26,7 +26,7 @@ export default function Home() {
             </span>
         </div>
         <div className={styles.tech}>
-          <h2>What I am</h2>
+          <h2>About me:</h2>
           <section>
                 <h3>Fullstack developer and Technical writing</h3>
                 <h2>I am a fullstack developer with <span>3</span>years of hands-on experience. Having worked with <span className={styles.javascript}>JAVASCRIPT</span>, <span className={styles.react}>REACT</span>, <span className={styles.node}>NODEJS</span>, <span className={styles.express}>EXPRESSJS</span>, <span className={styles.next}>NEXTJS</span>, I have created applications that helped solve client's needs. </h2>
